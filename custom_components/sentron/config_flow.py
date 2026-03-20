563740f0a93cb81d17e326e1d4aff9ba8626191a
fmoebis <fmoebis@gmail.com>
2026-03-20 19:35:33 +0000

    FIX

--- custom_components/sentron/config_flow.py
+++ custom_components/sentron/config_flow.py
@@ -1,21 +1,34 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_HOST, CONF_PORT, CONF_REGISTER
+from homeassistant.const import CONF_HOST, CONF_PORT
 
-class SentronConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+from .const import DOMAIN
+
+CONF_REGISTER = "register"
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
             return self.async_create_entry(
                 title=f"SENTRON {user_input[CONF_HOST]}",
-                data=user_input
+                data=user_input,
             )
 
-        schema = vol.Schema({
-            vol.Required(CONF_HOST): str,
-            vol.Required(CONF_PORT, default=502): int,
-            vol.Required(CONF_REGISTER, default=0): int,
-        })
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_HOST): str,
+                vol.Optional(CONF_PORT, default=502): int,
+                vol.Required(CONF_REGISTER, default=0): int,
+            }
+        )
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
+        )
