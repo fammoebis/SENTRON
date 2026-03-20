82e243c3465f2b3b987e0ed849e2f4cb9cef6296
fmoebis <fmoebis@gmail.com>
2026-03-20 20:55:22 +0000

    fix

--- custom_components/sentron/__init__.py
+++ custom_components/sentron/__init__.py
@@ -2,14 +2,18 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
 
+DOMAIN = "sentron"
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Setzt die Integration beim Start eines Eintrags auf."""
     hass.data.setdefault(DOMAIN, {})
+    
+    # Registriert die Plattformen (sensor.py)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Entlädt die Integration."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
